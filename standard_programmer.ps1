2f3634629793c327b3d8ffdaa5b1b8cbec1aaa76
Harald Dahl <harald@dira.no>
2019-08-29 12:19:34 +0200

    Split install to a separate file

--- standard_programmer.ps1
+++ standard_programmer.ps1
@@ -1,7 +1,3 @@
-# Made by Harald
-# Make sure to inspect https://chocolatey.org/install.ps1 before running this script to verify it is safe
-
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco upgrade chocolatey -y
 
 choco install googlechrome -y
